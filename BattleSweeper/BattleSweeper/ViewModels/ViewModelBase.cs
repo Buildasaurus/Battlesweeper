7af7813a6ef458e694c777ae6fa6e7a69e8e5077
karstensensensen <61250140+karstensensensen@users.noreply.github.com>
2023-02-09 09:24:24 +0100

    created solution file

---
+++ BattleSweeper/BattleSweeper/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleSweeper.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
