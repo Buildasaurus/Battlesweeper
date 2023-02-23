5a87ee2de5b56fd05e3d6b4912a9369bb140573d
Jonathan <Jonathan.dyrekilde.sommerlund@gmail.com>
2023-02-23 11:54:49 +0100

    Created Model Structure for game

---
+++ BattleSweeper/BattleSweeper/Views/MineSweeperView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace BattleSweeper.Views
+{
+    public partial class MineSweeperView : UserControl
+    {
+        public MineSweeperView()
+        {
+            InitializeComponent();
+        }
+    }
+}
