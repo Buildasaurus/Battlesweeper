4cac80cd783946ca37e0c14418fca2cef40f6dc0
karstensensensen <61250140+karstensensensen@users.noreply.github.com>
2023-02-23 10:12:28 +0100

    added some comments, and made MineSweeperTemplate better.

--- BattleSweeper/MineSweeper/MineSweeper/IMineSweeper.cs
+++ BattleSweeper/MineSweeper/MineSweeper/IMineSweeper.cs
@@ -2,17 +2,37 @@
 
 namespace Games.MineSweeper
 {
+    /// <summary>
+    /// represents a tile in a minesweeper grid.
+    /// 
+    /// stores the current bomb count (or if the tile is a bomb)
+    /// the revealed status and the flag status.
+    /// 
+    /// </summary>
     public class MineSweeperTile : ICopyable<MineSweeperTile>
     {
+        /// <summary>
+        /// construct MineSweeperTile and set the bomb_count to the passed parameter.
+        /// </summary>
+        /// <param name="bomb_count"></param>
         public MineSweeperTile(int bomb_count)
         {
             this.bomb_count = bomb_count;
         }
 
 
-        // -1 = bomb
+       /// <summary>
+       /// the number of bombs within a 1 tile radius, of the current tile.
+       /// -1 == the current tile is a bomb.
+       /// </summary>
         public int bomb_count { get; set; }
+        /// <summary>
+        /// if true, the tile should be rendered as a revealed tile, and the bomb_count should be displayed to the user.
+        /// </summary>
         public bool is_revealed;
+        /// <summary>
+        /// if true, the tile should not be able to be clicked.
+        /// </summary>
         public bool is_flagged;
 
         public MineSweeperTile Copy()
@@ -24,16 +44,45 @@ namespace Games.MineSweeper
             return tile;
         }
     }
+
+    /// <summary>
+    /// interface of a MineSweeper implementation.
+    /// 
+    /// exposes all the required fields, in order to implement a complete diffusable MineSweeper implementation.
+    /// 
+    /// </summary>
     public interface IMineSweeper
     {
+        /// <summary>
+        /// grid of tiles in the current minesweeper game.
+        /// </summary>
         public Grid<MineSweeperTile>? Tiles { get; }
+        /// <summary>
+        /// list of the initial bomb placements.
+        /// </summary>
         public HashSet<Point> InitialBombs { get; }
+        /// <summary>
+        /// list of the current bombs, that have not yet be diffused.
+        /// </summary>
         public HashSet<Point> CurrentBombs { get; }
 
-
+        /// <summary>
+        /// initialize a minesweeper game, of the specified size, and a specified bomb count.
+        /// </summary>
+        /// <param name="size"> must be greater than 0 X 0</param>
+        /// <param name="bombs"> muse be greater than -1 </param>
         public void generate(Size size, int bombs);
+        /// <summary>
+        /// attempts to reveal the tile at the specified coord.
+        /// </summary>
         public MoveResult testTile(Point coord);
+        /// <summary>
+        /// attempts to flag a tile at the specified coord.
+        /// </summary>
         public MoveResult flagTile(Point coord);
+        /// <summary>
+        /// attempts to diffuse a bomt at the specified coord.
+        /// </summary>
         public MoveResult diffuseTile(Point coord);
     }
 }
--- BattleSweeper/MineSweeper/MineSweeper/MineSweeperTemplate.cs
+++ BattleSweeper/MineSweeper/MineSweeper/MineSweeperTemplate.cs
@@ -7,56 +7,80 @@ using System.Threading.Tasks;
 
 namespace Games.MineSweeper
 {
+    /// <summary>
+    /// use place, setInitialBombs and setCurrentBombs to configure a simple IMineSweeper instance,
+    /// that can be used to develop the UI.
+    /// </summary>
     public class MineSweeperTemplate : IMineSweeper
     {
         public Grid<MineSweeperTile>? Tiles => m_grid;
 
-        public HashSet<Point> InitialBombs => throw new NotImplementedException();
+        public HashSet<Point> InitialBombs => m_initial_bombs;
 
-        public HashSet<Point> CurrentBombs => throw new NotImplementedException();
+        public HashSet<Point> CurrentBombs => m_current_bombs;
 
+        /// <summary>
+        /// sets the MineSweeperTile.is_revealed to true, at coord.
+        /// </summary>
+        /// <param name="coord"> MoveResult.Success</param>
         public MoveResult diffuseTile(Point coord)
         {
             Tiles![coord].is_revealed = true;
 
-            if (Tiles[coord].bomb_count == -1)
-            {
-                m_current_bombs.Remove(coord);
-            }
-
             return MoveResult.Success;
         }
 
+
+        /// <summary>
+        /// sets the MineSweeperTile.is_flagged to true, at coord.
+        /// </summary>
+        /// <param name="coord"> MoveResult.Success </param>
         public MoveResult flagTile(Point coord)
         {
             Tiles![coord].is_flagged = true;
             return MoveResult.Success;
         }
 
+        /// <summary>
+        /// sets the MineSweeperTile.is_revealed to true, at coord.
+        /// </summary>
+        /// <param name="coord"> MoveResult.Success </param>
+        public MoveResult testTile(Point coord)
+        {
+            Tiles![coord].is_revealed = true;
+            return MoveResult.Success;
+        }
+
+        /// <summary>
+        /// bombs are ignored here, use place() in order to place bombs.
+        /// </summary>
         public void generate(Size size, int bombs)
         {
             m_grid = new(size, new(0));
         }
 
+        /// <summary>
+        /// sets the MineSweeperTile at coord, to tile
+        /// </summary>
         public void place(Point coord, MineSweeperTile tile)
         {
             m_grid[coord] = tile.Copy();
         }
 
+        /// <summary>
+        /// set the initial bomb hash set data.
+        /// </summary>
         public void setInitialBombs(HashSet<Point> bombs)
         {
             m_initial_bombs = bombs;
         }
 
+        /// <summary>
+        /// set the current bomb hash set data.
+        /// </summary>
         public void setCurrentBombs(HashSet<Point> bombs)
         {
             m_current_bombs = bombs;
-        }
-
-        public MoveResult testTile(Point coord)
-        {
-            Tiles![coord].is_revealed = true;
-            return MoveResult.Success;
         }
 
         protected Grid<MineSweeperTile>? m_grid;
