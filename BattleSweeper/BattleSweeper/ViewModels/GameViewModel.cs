16cea2115c59d5969db9ef5fe81dc72e025659c4
karstensensensen <61250140+karstensensensen@users.noreply.github.com>
2023-03-09 14:11:49 +0100

    added auto completing game

--- BattleSweeper/BattleSweeper/ViewModels/GameViewModel.cs
+++ BattleSweeper/BattleSweeper/ViewModels/GameViewModel.cs
@@ -20,9 +20,48 @@ namespace BattleSweeper.ViewModels
 
         public GameViewModel()
         {
-            GameModel _game = new GameModel();
-            IMineSweeper mine_sweeper_model = MineSweeperFactory.construct<MineSweeper>(new(10, 10), 10);
+            IMineSweeper mine_sweeper_model = MineSweeperFactory.construct<MineSweeper>(new(10, 10), 10, 13);
 
+            MineSweeperViewModel mine_sweeper_vm = new(mine_sweeper_model, 60);
+            mine_sweeper_vm.start();
+
+            GameView = mine_sweeper_vm;
+
+            mine_sweeper_vm.leftClickTile(new Point(3, 3));
+            mine_sweeper_vm.leftClickTile(new Point(6, 6));
+            mine_sweeper_vm.leftClickTile(new Point(0, 2));
+            mine_sweeper_vm.leftClickTile(new Point(0, 0));
+            mine_sweeper_vm.leftClickTile(new Point(1, 0));
+            mine_sweeper_vm.leftClickTile(new Point(2, 0));
+            mine_sweeper_vm.leftClickTile(new Point(4, 0));
+            mine_sweeper_vm.leftClickTile(new Point(5, 1));
+            mine_sweeper_vm.leftClickTile(new Point(6, 0));
+            mine_sweeper_vm.leftClickTile(new Point(6, 1));
+            mine_sweeper_vm.leftClickTile(new Point(6, 2));
+            mine_sweeper_vm.leftClickTile(new Point(7, 1));
+            mine_sweeper_vm.leftClickTile(new Point(7, 2));
+            mine_sweeper_vm.leftClickTile(new Point(9, 3));
+            mine_sweeper_vm.leftClickTile(new Point(9, 4));
+            mine_sweeper_vm.leftClickTile(new Point(8, 4));
+            mine_sweeper_vm.leftClickTile(new Point(9, 5));
+            mine_sweeper_vm.leftClickTile(new Point(8, 5));
+            mine_sweeper_vm.leftClickTile(new Point(7, 6));
+            mine_sweeper_vm.leftClickTile(new Point(9, 6));
+            mine_sweeper_vm.leftClickTile(new Point(7, 7));
+            mine_sweeper_vm.leftClickTile(new Point(8, 7));
+            mine_sweeper_vm.leftClickTile(new Point(9, 7));
+            mine_sweeper_vm.leftClickTile(new Point(7, 8));
+            mine_sweeper_vm.leftClickTile(new Point(9, 8));
+            mine_sweeper_vm.rightClickTile(new Point(0, 9));
+            mine_sweeper_vm.rightClickTile(new Point(0, 1));
+            mine_sweeper_vm.rightClickTile(new Point(0, 3));
+            mine_sweeper_vm.rightClickTile(new Point(3, 0));
+            mine_sweeper_vm.rightClickTile(new Point(5, 0));
+            mine_sweeper_vm.rightClickTile(new Point(5, 2));
+            mine_sweeper_vm.rightClickTile(new Point(5, 6));
+            mine_sweeper_vm.rightClickTile(new Point(7, 9));
+            mine_sweeper_vm.rightClickTile(new Point(8, 3));
+            mine_sweeper_vm.rightClickTile(new Point(8, 6));
         }
         
         protected ViewModelBase m_game_view;
