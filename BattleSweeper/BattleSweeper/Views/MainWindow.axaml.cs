b38bbecbc93fc11543ca7f19e3b06afb13126bb3
karstensensensen <61250140+karstensensensen@users.noreply.github.com>
2023-03-09 14:12:33 +0100

    minesweeper view is now scaled 1:1

--- BattleSweeper/BattleSweeper/Views/MainWindow.axaml.cs
+++ BattleSweeper/BattleSweeper/Views/MainWindow.axaml.cs
@@ -11,7 +11,7 @@ namespace BattleSweeper.Views
 
             Closing += (s, e) =>
             {
-                GameViewModel.stop();
+                //GameViewModel.stop();
             };
         }
 
