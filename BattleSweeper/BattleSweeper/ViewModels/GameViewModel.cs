48c1c6127fc59330d2ffa40598e0bcb04e706630
Jonathan <Jonathan.dyrekilde.sommerlund@gmail.com>
2023-03-09 11:48:51 +0100

    finite while loop

--- BattleSweeper/BattleSweeper/ViewModels/GameViewModel.cs
+++ BattleSweeper/BattleSweeper/ViewModels/GameViewModel.cs
@@ -8,49 +8,21 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Diagnostics;
 
 namespace BattleSweeper.ViewModels
 {
     public class GameViewModel : ViewModelBase
     {
-        [DllImport("user32.dll")]
-        static extern bool GetCursorPos(ref Point lpPoint);
-
-        private Thread mouseThread;
-        private bool isRunning = true;
+        
 
         public GameViewModel()
         {
             GameModel _game = new GameModel();
             IMineSweeper mine_sweeper_model = MineSweeperFactory.construct<MineSweeper>(new(10, 10), 10);
 
-
-            // Create a new thread to continuously monitor the mouse for input.
-            mouseThread = new Thread(new ThreadStart(MouseThread));
-            mouseThread.Start();
-            MouseThread();
         }
 
-        private void MouseThread()
-        {
-            while (isRunning)
-            {
-                // Get the current mouse position.
-                Point cursorPos = new Point();
-                GetCursorPos(ref cursorPos);
-
-                // Do something with the mouse position.
-                Console.WriteLine($"Mouse position: {cursorPos.X}, {cursorPos.Y}");
-
-                // Sleep for a short amount of time to prevent the loop from running too frequently.
-                Thread.Sleep(10);
-            }
-        }
-        // Call this method to stop the thread and terminate the loop.
-        public void Stop()
-        {
-            isRunning = false;
-            mouseThread.Join();
-        }
+        
     }   
 }
