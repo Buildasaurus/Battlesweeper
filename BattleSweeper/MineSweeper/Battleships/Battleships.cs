c4624e6c1ef75e5be3e6e009e7251cd0be8e2b49
Peter Stentoft <pete186f@elev.tec.dk>
2023-03-23 09:13:53 +0100

    Placeship Done and shoot ocmmand begun

--- BattleSweeper/MineSweeper/Battleships/Battleships.cs
+++ BattleSweeper/MineSweeper/Battleships/Battleships.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 namespace Games.Battleships
 {
 
+    
     public class Battleships
     {
         
@@ -15,23 +16,67 @@ namespace Games.Battleships
         Grid<BattleshipTile> Board; 
         List<int> shipLengths = new List<int>();
         int n = 0;
-        public MoveResult placeShip(Point point, bool Vertical)
+
+        /*public MoveResult shoot(Point coord)
         {
-            for (int i = 1; i < shipLengths[n]; i++) {
+            if (Board[coord].hasBeenShot == true)
+            {
+                return MoveResult.Illegal;
+            }
+            if (Board[coord].hasBomb == true)
+            {
+                for (int dx = -1; dx <= 1; dx++)
+                {
+                    for (int dy = -1; dy <= 1; dy++)
+                    {
+                        Point delta_coord = new(coord.X + dx, coord.Y + dy);
 
-                if (Vertical == true) { 
-
-                    if (Board[point.Y+i,point.X].ship > 1) {
-                    return MoveResult.Illegal;
+                            shoot(delta_coord);
+                        
                     }
                 }
-                else {
-                    if (Board[point.Y, point.X+i].ship > 0) {
+                return MoveResult.Success;
+            }
+        
+        }
+        */
+        public MoveResult placeShip(Point coord, bool Vertical)
+        {
+
+            if (Vertical == true) 
+            {
+                if (shipLengths[n] + coord.Y > 10)
+                    return MoveResult.Illegal;
+
+                for (int i = 0; i < shipLengths[n]; i++)
+                {
+                    if (Board[coord.Y + i, coord.X].ship > 0)
                         return MoveResult.Illegal;
-                    }
                 }
             }
-           
+                
+            else {
+                if (shipLengths[n] + coord.X > 10)
+                    return MoveResult.Illegal;
+
+                    for (int i = 0; i < shipLengths[n]; i++)
+                {
+                    if (Board[coord.Y, coord.X + i].ship > 0)
+                        return MoveResult.Illegal;
+                }
+            }
+
+            if (Vertical == true)
+            {
+                for (int i = 0; i < shipLengths[n]; i++)
+                    Board[coord.Y, coord.X + i].ship = n;
+            }
+
+            else
+            {
+                for (int i = 0; i < shipLengths[n]; i++)
+                    Board[coord.Y + 1, coord.X].ship = n;
+            }
 
             n++;
             return MoveResult.Success;
