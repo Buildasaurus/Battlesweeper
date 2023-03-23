797f7e253441d89a86bdfaf7f0e9893ee4ea2e78
Jonathan <Jonathan.dyrekilde.sommerlund@gmail.com>
2023-03-23 09:55:44 +0100

    Now you can also move the window

--- BattleSweeper/BattleSweeper/ViewModels/GameViewModel.cs
+++ BattleSweeper/BattleSweeper/ViewModels/GameViewModel.cs
@@ -32,6 +32,7 @@ namespace BattleSweeper.ViewModels
 
         static Window window = (App.Current.ApplicationLifetime as ClassicDesktopStyleApplicationLifetime).MainWindow;
         public bool shiftPressed = false;
+        int minesweepergame = 1;
         public GameViewModel()
         {
             EventHandler.start();
@@ -40,6 +41,8 @@ namespace BattleSweeper.ViewModels
             IMineSweeper mine_sweeper_model = MineSweeperFactory.construct<MineSweeper>(gridSize, 10);
             //create the view model, with a 60 second timer.
             MineSweeperViewModel mine_sweeper_vm = new(mine_sweeper_model, 60);
+            mine_sweeper_vm.GameOver += gameover;
+
             EventHandler.KeyChanged += (x =>
             {
                 if(x.key == Key.LeftShift)
@@ -71,12 +74,6 @@ namespace BattleSweeper.ViewModels
                         }
                     }
                 }
-
-                /*Get the current mouse position.
-                Point cursorPos = new Point();
-                GetCursorPos(ref cursorPos);
-                mousePosition = new Point(cursorPos.X, cursorPos.Y);
-                MouseChanged?.Invoke(new MouseArgs((MouseButton)c.Type, mousePosition));*/
             });
             
 
@@ -86,7 +83,18 @@ namespace BattleSweeper.ViewModels
             GameView = mine_sweeper_vm;
         }
 
-        
+        public void gameover (object? s, bool foundAllBombs)
+        {
+            if (minesweepergame == 1) //if first game, start the next one
+            {
+
+            }
+        }
+
+        void constructMineField()
+        {
+
+        }
 
         public System.Drawing.Point coordToField(Rect gridCoord, System.Drawing.Point mousePos)
         {
@@ -97,8 +105,8 @@ namespace BattleSweeper.ViewModels
             double y;
 
             //calc x and y coordinat relative to topleft
-            x = mousePos.X / scale - gridCoord.TopLeft.X - windowX;
-			y = mousePos.Y / scale - gridCoord.TopLeft.Y - windowY;
+            x = mousePos.X / scale - gridCoord.TopLeft.X - windowX / scale;
+			y = mousePos.Y / scale - gridCoord.TopLeft.Y - windowY / scale;
             Trace.WriteLine(gridCoord.TopLeft.X);
 
             //calculate corresponding tile
