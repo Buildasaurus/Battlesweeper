9ed744464a815af3a0ad9557dd9712862a03a9c0
karstensensensen <61250140+karstensensensen@users.noreply.github.com>
2023-03-28 10:39:10 +0200

    added battle ship viewmodel

---
+++ BattleSweeper/BattleSweeper/ViewModels/BattleShipViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Games.Battleships;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleSweeper.ViewModels
+{
+    public class BattleShipsViewModel : ViewModelBase
+    {
+        public BattleShipsViewModel(IBattleships battleships)
+        {
+            this.battleships = battleships;
+        }
+
+        public bool Player1Playing { get; }
+        public bool Player2Playing { get; }
+
+        public IBattleships battleships;
+    }
+}
