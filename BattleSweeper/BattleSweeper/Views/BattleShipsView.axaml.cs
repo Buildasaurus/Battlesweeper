c7c4ebebacf6c96d26874f0a14537f9e547d7f6a
karstensensensen <61250140+karstensensensen@users.noreply.github.com>
2023-03-31 10:16:47 +0200

    added Player enum

--- BattleSweeper/BattleSweeper/Views/BattleShipsView.axaml.cs
+++ BattleSweeper/BattleSweeper/Views/BattleShipsView.axaml.cs
@@ -1,5 +1,7 @@
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Presenters;
+using Avalonia.Data;
 using BattleSweeper.ViewModels;
 using DynamicData;
 using Games.Battleships;
@@ -21,18 +23,36 @@ namespace BattleSweeper.Views
             {
                 Grid player_1_grid = this.FindControl<Grid>("Player1Grid");
 
-                setupBattleshipsGrid(player_1_grid, vm.bs_player_1, ref vm.bs1_tile_vm);
+                var bounds_1_binding = new Binding
+                {
+                    Source = vm,
+                    Path = nameof(vm.Bs1TransformedBounds),
+                    Mode = BindingMode.OneWayToSource
+                };
+
+                player_1_grid.Bind(TransformedBoundsProperty, bounds_1_binding);
+
+                setupBattleshipsGrid(player_1_grid, vm.bs_player_1, vm, Player.Player1, ref vm.bs1_tile_vm);
 
                 Grid player_2_grid = this.FindControl<Grid>("Player2Grid");
 
-                setupBattleshipsGrid(player_2_grid, vm.bs_player_2, ref vm.bs2_tile_vm);
+                var bounds_2_binding = new Binding
+                {
+                    Source = vm,
+                    Path = nameof(vm.Bs2TransformedBounds),
+                    Mode = BindingMode.OneWayToSource
+                };
+
+                player_2_grid.Bind(TransformedBoundsProperty, bounds_2_binding);
+
+                setupBattleshipsGrid(player_2_grid, vm.bs_player_2, vm, Player.Player2, ref vm.bs2_tile_vm);
             }
         }
 
         /// <summary>
         /// constructs a viewmodel grid, for the passed battleships model, and binds it to an avalonia grid.
         /// </summary>
-        private void setupBattleshipsGrid(Grid grid, IBattleships battle_ships, ref Games.Grid<BSTileVM> tiles_viewmodel)
+        private void setupBattleshipsGrid(Grid grid, IBattleships battle_ships, BattleShipsViewModel vm, Player player, ref Games.Grid<BSTileVM> tiles_viewmodel)
         {
             for (int x = 0; x < battle_ships.Tiles.Size.Width; x++)
             {
@@ -42,7 +62,7 @@ namespace BattleSweeper.Views
                     if(y == 0)
                         grid.RowDefinitions.Add(new RowDefinition(1, GridUnitType.Star));
 
-                    tiles_viewmodel[x, y] = new(battle_ships.Tiles[x, y]);
+                    tiles_viewmodel[x, y] = new(battle_ships.Tiles[x, y], vm, player);
 
                     ContentControl presenter = new()
                     {
