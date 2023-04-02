25d0a2623673a737b7efcdf09393798c0cfbf668
Buildasaurus <jonathan.dyrekilde.sommerlund@gmail.com>
2023-04-02 18:55:51 +0200

    Added endscreen

---
+++ BattleSweeper/BattleSweeper/ViewModels/EndScreenViewModel.cs
@@ -0,0 +1,24 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleSweeper.ViewModels
+{
+	public class EndScreenViewModel : ViewModelBase
+	{
+		public EndScreenViewModel(string textmessage)
+		{
+			NewGame = ReactiveCommand.Create(() => { });
+			Text = textmessage;
+			Button = "Start over";
+		}
+		public string Text { get; set; }
+		public string Button { get; set; }
+		public ReactiveCommand<Unit, Unit> NewGame { get; set; }
+	}
+
+}
