37686b28472c23c8df160bbc5c85436110ad6361
Buildasaurus <jonathan.dyrekilde.sommerlund@gmail.com>
2023-04-03 14:40:22 +0200

    Dynamic boat lenghts in top bar

--- BattleSweeper/BattleSweeper/Views/BattleShipsView.axaml.cs
+++ BattleSweeper/BattleSweeper/Views/BattleShipsView.axaml.cs
@@ -2,10 +2,23 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Presenters;
 using Avalonia.Data;
+using Avalonia.Layout;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
 using BattleSweeper.ViewModels;
 using DynamicData;
 using Games.Battleships;
 using ReactiveUI;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using ColumnDefinition = Avalonia.Controls.ColumnDefinition;
+using ContentControl = Avalonia.Controls.ContentControl;
+using Grid = Avalonia.Controls.Grid;
+using Image = Avalonia.Controls.Image;
+using Orientation = Avalonia.Layout.Orientation;
+using RowDefinition = Avalonia.Controls.RowDefinition;
+using StackPanel = Avalonia.Controls.StackPanel;
+using UserControl = Avalonia.Controls.UserControl;
 
 namespace BattleSweeper.Views
 {
@@ -21,7 +34,9 @@ namespace BattleSweeper.Views
         {
             if (DataContext is BattleShipsViewModel vm)
             {
-                Grid player_1_grid = this.FindControl<Grid>("Player1Grid");
+				setupShipsDisplay(vm);
+
+				Grid player_1_grid = this.FindControl<Grid>("Player1Grid");
 
                 var bounds_1_binding = new Binding
                 {
@@ -76,6 +91,43 @@ namespace BattleSweeper.Views
                 }
             }
         }
+
+        private void setupShipsDisplay(BattleShipsViewModel vm)
+        {
+            List<int> shiplenghts = vm.bs_player_1.shipLengths;
+            int counter = 0;
+            foreach (int shiplength in shiplenghts)
+            {
+                StackPanel stk = new StackPanel();
+                stk.Orientation = Orientation.Horizontal;
+                string binding = "ShipHighlights[" + counter + "]";
+				stk.Bind(StackPanel.BackgroundProperty, new Binding(binding));
+                counter++;
+				for (int i = 0; i < shiplength; i++) //add middle parts
+                {
+					double? width = this.Resources["width"] as double?;
+					Image img = new Image();
+					img.Width = width ?? default(double);
+					if (i == shiplength - 1) // if placing last ship, rotation is different
+                    {
+						img.RenderTransform = new RotateTransform(270);
+                        img.Source = (this.Resources["end"] as Image).Source;
+					}
+					else
+                    {
+						img.RenderTransform = new RotateTransform(90);
+						img.Source = (this.Resources["mid"] as Image).Source;
+					}
+					if (i == 0)
+                    {
+						img.Source = (this.Resources["end"] as Image).Source;
+					}
+					stk.Children.Add(img);
+				}
+				shipDisplay.Children.Add(stk);
+
+			}
+		}
 
     }
 }
